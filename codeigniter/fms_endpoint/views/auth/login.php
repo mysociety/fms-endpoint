85040d3959cf8d7ccdf46f13653f6450f1383669
Dave Whiteland <dave@mysociety.org>
2012-10-02 08:35:12 +0100

    improved ugly-pugly login form and removed misleading word 'username', since this is all by email address currently

--- codeigniter/fms_endpoint/views/auth/login.php
+++ codeigniter/fms_endpoint/views/auth/login.php
@@ -2,33 +2,28 @@
 <div class='mainInfo'>
 <h2>Login</h2>
 <div class="pageTitleBorder"></div>
-<p>Please login with your email/username and password below.</p>
+<p>Please login with your email address and password below.</p>
 
 <div id="infoMessage"><?php echo $message;?></div>
-
-<?php echo form_open("auth/login");?>
-
-<p>
-<label for="identity">Email/Username:</label>
-<?php echo form_input($identity);?>
-</p>
-
-<p>
-<label for="password">Password:</label>
-<?php echo form_input($password);?>
-</p>
-
-<p>
-<label for="remember">Remember Me:</label>
-<?php echo form_checkbox('remember', '1', FALSE, 'id="remember"');?>
-</p>
-
-
-<p><?php echo form_submit('submit', 'Login');?></p>
-
-
-<?php echo form_close();?>
-
+<div class="login-form">
+	<?php echo form_open("auth/login");?>
+		<div class="login-identity">
+			<label for="identity">Email address:</label>
+			<?php echo form_input($identity);?>
+		</div>
+		<div class="login-password">
+			<label for="password">Password:</label>
+			<?php echo form_input($password);?>
+		</div>
+		<div class="login-remember">
+			<label for="remember">Remember Me:</label>
+			<?php echo form_checkbox('remember', '1', FALSE, 'id="remember"');?>
+		</div>
+		<div class="login-submit">
+			<?php echo form_submit('submit', 'Login');?>
+		</div>
+	<?php echo form_close();?>
+</div>
 <p><a href="forgot_password">Forgot your password?</a></p>
 
 </div>
