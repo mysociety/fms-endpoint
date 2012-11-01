6f0c8857ac83bb90ac3005279510f5a2f1e20a1f
Dave Whiteland <dave@mysociety.org>
2012-11-01 21:31:30 +0000

    add request_updates table to schema and provide migration file

--- db/fms-endpoint-initial.sql
+++ db/fms-endpoint-initial.sql
@@ -260,6 +260,24 @@ CREATE TABLE `api_keys` (
 
 INSERT INTO `api_keys` VALUES('12345', 1, 'don\'t use -- delete when live');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `request_updates`
+--
+
+CREATE TABLE `request_updates` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `report_id` int(11) NOT NULL,
+  `status_id` int(11) NOT NULL,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `update_desc` text,
+  `old_status_id` int(11) DEFAULT NULL,
+  `changed_by` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `report_id` (`report_id`,`status_id`,`updated_at`),
+  KEY `changed_by` (`changed_by`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
