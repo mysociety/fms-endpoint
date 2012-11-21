cc15c52030ab84d9d9bbca916d29199367a96a8f
Dave Whiteland <dave@mysociety.org>
2012-11-21 19:50:17 +0000

    fixed non-compliant XML node name: should be request_update

--- codeigniter/fms_endpoint/views/request_update_post_response_xml.php
+++ codeigniter/fms_endpoint/views/request_update_post_response_xml.php
@@ -3,7 +3,7 @@
 $this->load->helper('xml');
 $dom = xml_dom();
 $updates = xml_add_child($dom, 'service_request_updates');
-$update = xml_add_child($updates, 'update');
+$update = xml_add_child($updates, 'request_update');
 xml_add_child($update, 'update_id', $new_update_id);
 
 xml_print($dom);
