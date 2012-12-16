473450ded04d388195334cbe5cde49b8b4723b25
Dave Whiteland <dave@mysociety.org>
2012-12-16 20:08:22 +0000

    add capability to link to the org/department's reports summary page (map) on FMS

--- codeigniter/fms_endpoint/views/footer.php
+++ codeigniter/fms_endpoint/views/footer.php
@@ -1,5 +1,18 @@
   </div>
   <div class="fmse-footer">
+	<?php 
+		$org_url = config_item('organisation_url');
+		if (! empty($org_url)) { ?>
+			<div class="departmental-link">
+				<a href="<?php echo $org_url; ?>" class="<?php echo (preg_match('/https?:\/\/(\\w*\\.)*fixmy/', $org_url))? 'fmse-web-link-fms':'fmse-web-link' ?>"><?php 
+				if (config_item('organisation_link_text')) {
+					echo config_item('organisation_link_text');
+				} else {
+					echo $org_url;
+				}
+			?></a>
+		</div>
+	<?php } ?>
     <div class="open311-status">
       <a href="/admin/open311" 
         class="open311-status-<?php if(is_config_true(config_item('enable_open311_server'))) { echo('on');} else {echo('off');} ?>">
